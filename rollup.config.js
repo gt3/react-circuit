b3708f1391e642c34223f4e830dac7d415337075
Ankit Patel <gt3.someday@outlook.com>
2017-07-18 21:54:44 -0500

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,22 @@
 import babel from 'rollup-plugin-babel'
-import babili from 'rollup-plugin-babili'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
-const minify = process.env.DIST === 'true'
+//const minify = process.env.DIST === 'true'
 
 const lib = {
   entry: './src/index.js',
+  globals: { 'react': 'react', 'prop-types': 'PropTypes', 'js-csp': 'csp' },
+  external: ['react','js-csp','prop-types'],
   plugins: [
+    resolve({}),
+    commonjs({}),
     babel({exclude: 'node_modules/**'})
   ],
   targets: [
-    { format: 'es', dest: './lib/react-circuit.mjs' },
-    { format: 'umd', dest: './lib/react-circuit.js', moduleId: 'react-circuit', moduleName: 'Circuit' }
+    { format: 'es', dest: './lib/react-circuit.es.js' },
+    { format: 'umd', dest: './lib/react-circuit.js', moduleId: 'react-circuit', moduleName: 'circuit' }
   ]
 }
 
-const dist = {
-  entry: './lib/react-circuit.js',
-  plugins: [
-    babili()
-  ],
-  targets: [
-    { format: 'umd', dest: './dist/react-circuit.min.js', moduleId: 'react-circuit', moduleName: 'Circuit' }
-  ]
-}
-
-export default minify ? dist: lib
+export default lib
