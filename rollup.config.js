4fc9a362ecadefd0a0d0ab48006c7566135884b8
Ankit Patel <gt3.someday@outlook.com>
2017-07-18 22:20:00 -0500

    set environment to development

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-
+import replace from 'rollup-plugin-replace'
 //const minify = process.env.DIST === 'true'
 
 const lib = {
@@ -9,9 +9,10 @@ const lib = {
   globals: { 'react': 'react', 'prop-types': 'PropTypes', 'js-csp': 'csp' },
   external: ['react','js-csp','prop-types'],
   plugins: [
+    babel({exclude: 'node_modules/**'}),
+    replace({'process.env.NODE_ENV': JSON.stringify('development')}),
     resolve({}),
-    commonjs({}),
-    babel({exclude: 'node_modules/**'})
+    commonjs({})
   ],
   targets: [
     { format: 'es', dest: './lib/react-circuit.es.js' },
